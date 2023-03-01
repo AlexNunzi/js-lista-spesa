70ec04eaf72b32f91ea5ab3053fb513db11a95c5
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-01 17:18:24 +0100

    Aggiunta logica di inserimento contenuti dell'array nella ul dell'html tramite ciclo while

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const listContainerDom = document.querySelector('#shoppingListContainer');
 const shoppingList = [
     'fragole', 
     'mele', 
@@ -7,3 +8,22 @@ const shoppingList = [
 ];
 
 console.log(shoppingList);
+
+let counter = 0;
+
+while(shoppingList.length > counter){
+    const listElement = document.createElement('li');
+    listElement.append(shoppingList[counter]);
+    listContainerDom.append(listElement);
+    counter++
+}
+
+// Versione utilizzando il ciclo do while 
+// do{
+//     const listElement = document.createElement('li');
+//     listElement.append(shoppingList[counter]);
+//     listContainerDom.append(listElement);
+//     counter++
+// } while (counter < shoppingList.length)
+
+
